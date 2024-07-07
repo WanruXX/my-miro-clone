40bac6730cc30e489551abc5c8772f7c69014b23
WanruXX <wanru.xia1@gmail.com>
2024-07-08 00:03:26 +0200

    add multiselection

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
-import { Camera, Color, Point, Side, XYHW } from "@/types/canvas";
+import { Camera, Color, Layer, Point, Side, XYHW } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
+import { read } from "fs";
 import { twMerge } from "tailwind-merge"
 
 const COLORS = [
@@ -56,3 +57,33 @@ export function resizeBounds(bounds: XYHW, corner: Side, point: Point): XYHW {
   }
   return result;
 };
+
+export function findIntersectingLayersWithRectangle(
+  layerIds: readonly string[],
+  layers: ReadonlyMap<string, Layer>,
+  a: Point,
+  b: Point,
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(b.x - a.x),
+    height: Math.abs(b.y - a.y),
+  }
+
+  const ids = [];
+
+  for (const LayerId of layerIds) {
+    const layer = layers.get(LayerId);
+    if (layer == null) {
+      continue;
+    }
+    const { x, y, height, width } = layer;
+
+    if (rect.x + rect.width > x && rect.x < x + width && rect.y + rect.height > y && rect.y < y + height) {
+      ids.push(LayerId);
+    }
+  }
+
+  return ids;
+}
