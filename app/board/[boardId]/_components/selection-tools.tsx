fe8405c00985fbeaec664755a2dca648c3899e4d
WanruXX <wanru.xia1@gmail.com>
2024-07-07 23:14:22 +0200

    add moving to front/back options to layers

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -8,7 +8,7 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
     camera: Camera;
@@ -19,6 +19,13 @@ export const SelectionTools = memo(({
     camera,
     setLastUsedColor,
 }: SelectionToolsProps) => {
+    const selectionBounds = useSelectionBounds();
+    if (!selectionBounds) {
+        return null;
+    }
+    const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+    const y = selectionBounds.y + camera.y;
+
     const selection = useSelf((me) => me.presence.selection);
 
     const setFill = useMutation((
@@ -32,13 +39,42 @@ export const SelectionTools = memo(({
 
     const deleteLayers = useDeleteLayers();
 
-    const selectionBounds = useSelectionBounds();
-    if (!selectionBounds) {
-        return null;
-    }
+    const moveToFront = useMutation((
+        { storage }
+    ) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
 
-    const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
-    const y = selectionBounds.y + camera.y;
+        const arr = liveLayerIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+                indices.push(i);
+            }
+        }
+        for (let i = indices.length - 1; i >= 0; i--) {
+            liveLayerIds.move(indices[i], arr.length - 1 - (indices.length - 1 - i));
+        }
+    }, [selection]);
+
+    const moveToBack = useMutation((
+        { storage }
+    ) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+                indices.push(i);
+            }
+        }
+        for (let i = 0; i < indices.length; i++) {
+            liveLayerIds.move(indices[i], i);
+        }
+    }, [selection]);
+
 
     return (
         <div
@@ -47,6 +83,18 @@ export const SelectionTools = memo(({
                 transform: `translate(calc(${x}px - 50%), calc(${y - 16}px - 100%))`
             }}>
             <ColorPicker onChange={setFill} />
+            <div className="flex flex-col gap-y-0.5">
+                <Hint label="Bring to front">
+                    <Button variant="board" size="icon" onClick={moveToFront}>
+                        <BringToFront />
+                    </Button>
+                </Hint>
+                <Hint label="Send to back">
+                    <Button variant="board" size="icon" onClick={moveToBack}>
+                        <SendToBack />
+                    </Button>
+                </Hint>
+            </div>
             <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
                 <Hint label="Delete">
                     <Button variant="board" size="icon" onClick={deleteLayers}>
