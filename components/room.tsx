5ef76afda0b8f149e8d9b7250863268a39c33b44
WanruXX <wanru.xia1@gmail.com>
2024-07-10 01:40:04 +0200

    add drawing with pencil

--- components/room.tsx
+++ components/room.tsx
@@ -18,6 +18,8 @@ export const Room = ({
             initialPresence={{
                 cursor: null,
                 selection: [],
+                pencilDraft: null,
+                penColor: null,
             }}
             initialStorage={{
                 layers: new LiveMap<string, LiveObject<Layer>>(),
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,7 +2,7 @@
 // https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 
 import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
-import { Layer } from "./types/canvas";
+import { Color, Layer } from "./types/canvas";
 
 
 declare global {
@@ -11,6 +11,8 @@ declare global {
     Presence: {
       cursor: { x: number; y: number } | null;
       selection: string[];
+      pencilDraft: [x: number, y: number, pressure: number][] | null;
+      penColor: Color | null;
     };
 
     // The Storage tree for the room, for useMutation, useStorage, etc.
